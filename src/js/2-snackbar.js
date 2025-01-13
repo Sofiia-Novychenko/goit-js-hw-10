a7e001df2fe68f27132e808b6d0c684133251fe3
Sofiia Novychenko <milakotiy@gmail.com>
2025-01-13 01:23:15 +0100

    add css markup fot task 2

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,13 @@
+iziToast.error({
+  messageColor: 'rgba(225, 225, 225, 1)',
+  messageSize: 16,
+  messageLineHeight: 1.5,
+  backgroundColor: 'rgba(239, 64, 64, 1)',
+  position: 'topRight',
+  message: 'Please choose a date in the future',
+});
+
+iziToast.success({
+  title: 'OK',
+  message: 'Successfully inserted record!',
+});
