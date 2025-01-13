f884a7241b12183b013d9dbf0f90dccf0d3b95a9
Sofiia-Novychenko <milakotiy@gmail.com>
2025-01-13 00:23:48 +0000

    Deploying to gh-pages from @ Sofiia-Novychenko/goit-js-hw-10@a7e001df2fe68f27132e808b6d0c684133251fe3 🚀

---
+++ 2-snackbar.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */iziToast.error({messageColor:"rgba(225, 225, 225, 1)",messageSize:16,messageLineHeight:1.5,backgroundColor:"rgba(239, 64, 64, 1)",position:"topRight",message:"Please choose a date in the future"});iziToast.success({title:"OK",message:"Successfully inserted record!"});
+//# sourceMappingURL=2-snackbar.js.map
